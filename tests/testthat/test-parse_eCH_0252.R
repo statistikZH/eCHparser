75d8367f22dfa5be48e0560e65164d7bf240fc6e
Simon Graf <simon.graf@statistik.ji.zh.ch>
2024-10-16 07:11:44 +0000

    first test for parse_eCH_0252()

--- tests/testthat/test-parse_eCH_0252.R
+++ tests/testthat/test-parse_eCH_0252.R
@@ -1,3 +1,16 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Output of parsed file is dataframe", {
+
+  testfiles <- system.file("extdata", package = "eCHparser") |>
+    list.files()
+
+  for (i in 1:length(testfiles)) {
+
+    filepath <- system.file("extdata", testfiles[i], package = "eCHparser")
+
+    file_out <- parse_eCH_0252(filepath)
+
+    testthat::expect_true(is.data.frame(file_out))
+
+  }
+
 })
