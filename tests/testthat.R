d57373c050a1a960a47f871856dbf522272b1b05
Simon Graf <simon.graf@statistik.ji.zh.ch>
2024-10-16 07:11:00 +0000

    renaming

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/articles/special-files.html
 
 library(testthat)
-library(eCHPaRserPolitics)
+library(eCHparser)
 
-test_check("eCHPaRserPolitics")
+test_check("eCHparser")
