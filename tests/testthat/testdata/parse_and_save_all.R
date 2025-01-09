9c027b988ac4b5e00c5bda0b5416db3361581f9c
Simon Graf <simon.graf@statistik.ji.zh.ch>
2025-01-09 10:54:26 +0000

    Data structure adjusted.

--- tests/testthat/testdata/parse_and_save_all.R
+++ tests/testthat/testdata/parse_and_save_all.R
@@ -7,19 +7,18 @@
 # PARSE ALL TEST FILES AND SAVE THEM ===========================================
 
 
-testfiles <- system.file("extdata", package = "eCHparser") |>
-  list.files()
+testfiles <- list.files(testthat::test_path("testdata/files_unparsed"))
 
 for (i in seq_along(testfiles)) {
 
   # Get the full file path
-  filepath <- system.file("extdata", testfiles[i], package = "eCHparser")
+  filepath <- testthat::test_path("testdata/files_unparsed", paste0(testfiles[i]))
 
   # Parse the file using the parse_eCH_0252 function
   file_out <- parse_eCH_0252(filepath)
 
   # Load the corresponding RDS file from the testdata folder (use testthat::test_path for the test to work also with devtools::test())
-  rds_filepath <- testthat::test_path("testdata", paste0(sub("\\.[^.]*$", "", testfiles[i]), ".RDS")) # the sub() removes the .xml extension
+  rds_filepath <- testthat::test_path("testdata/files_parsed", paste0(sub("\\.[^.]*$", "", testfiles[i]), ".RDS")) # the sub() removes the .xml extension
 
   saveRDS(file_out, rds_filepath)
 
