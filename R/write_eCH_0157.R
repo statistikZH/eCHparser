e682af4daf29745d4a661848668b67a8c4be7ec8
Simon Graf <simon.graf@statistik.ji.zh.ch>
2025-06-04 15:08:48 +0000

    Continue on writer 157

--- R/write_eCH_0157.R
+++ R/write_eCH_0157.R
@@ -14,15 +14,67 @@
 #'
 write_eCH_0157 <- function(file, template_xml_path = system.file("templates", "eCH_0157_template.xml", package = "eCHparser")){
 
+
+  # !!!!!!! DEV-HELPER - DELETE AFTER DEV  =====================================
+
+
+  # test <- parse_eCH_0157("tests/testthat/testdata/files_unparsed/eCH-0157/eCH-0157_abraxas_elections_ZH_majority_2026-06-16.xml")
+  # writexl::write_xlsx(test, "/home/file-server/01_Post/Graf/eCH-0157_abraxas_elections_ZH_majority_2026-06-16.xlsx")
+  # file <- "/home/file-server/01_Post/Graf/eCH-0157_abraxas_elections_ZH_majority_2026-06-16.xlsx"
+  # target_xml <- xml2::read_xml("tests/testthat/testdata/files_unparsed/eCH-0157/eCH-0157_abraxas_elections_ZH_majority_2026-06-16.xml")
+  # target_list <- xml2::as_list(target_xml)
+
+
+  # PREPARE DATA ===============================================================
+
+
   # Read xlsx file
-  data <- readxl::read_xlsx(file)
+  data <- readxl::read_xlsx(file) # This could/should be changed later so that the input is a tibble
 
-  # Split data into contest, election ballots, elections, and candidates
-  contest_tbl
-  election
+  # Split data into contest...
+  contest_tbl <- data |>
+    dplyr::select(contest_contestIdentification:`contestDescriptionInfo-rm_contestDescription`) |>
+    unique()
+
+  # ... election group ballots, ...
+  election_group_ballot_tbl <- data |>
+    dplyr::select(electionGroupBallot_domainOfInfluenceIdentification) |>
+    unique()
+
+  # ...elections, and...
+  election_tbl <- data |>
+    dplyr::select(
+      election_electionIdentification:candidate_candidateIdentification,
+      -candidate_candidateIdentification,
+      electionGroupBallot_domainOfInfluenceIdentification # as link to the election group ballot
+    ) |>
+    unique()
+
+  # ...candidates.
+  candidates_tbl <- data |>
+    dplyr::select(
+      candidate_candidateIdentification:ncol(data),
+      election_electionIdentification # as link to the election
+    )
 
 
-  unique(data$contest_contestIdentification)
+  # WRITE LIST =================================================================
+
+
+  # Candidates -----------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+  initialDelivery <- list()
+
+  initialDelivery <- append(initialDelivery, list("contest"))
+
 
 }
 
@@ -30,8 +82,115 @@ write_eCH_0157 <- function(file, template_xml_path = system.file("templates", "e
 
 
 
+#' Create candidate lists
+#'
+#' @description
+#' This helper function transforms candidate information to a candidate list.
+#'
+#' @param data A tibble containing the necessary candidate information for the eCH-0157.
+#'
+#' @return An list.
+#' @export
+#'
+#' @examples
+#'
+create_candidate_list <- function(data){
 
 
+  # !!!!!!! DEV-HELPER - DELETE AFTER DEV  =====================================
+
+
+  # data <- candidates_tbl[1, ]
+
+
+  # PREPARE NESTED LISTS =======================================================
+
+
+  # Define and transform nested data
+  data_nested <- data |>
+    dplyr::select(
+      dplyr::contains("-")
+    ) |>
+    tidyr::pivot_longer(
+      cols = everything(),
+      names_to = "name",
+      values_to = "value"
+    ) |>
+    dplyr::mutate(
+      language = sub(".*-([a-zA-Z]{2}).*", "\\1", name),
+      name_list_element = sub(".*_", "", name),
+      name_parent_element = sub("-.*", "", name)
+    )
+
+  # Create lists
+  list <- lapply(data_nested$name_parent_element |> unique(), function(list_name) {
+
+    # Filter data
+    data_nested_parent <- data_nested |>
+      dplyr::filter(name_parent_element == list_name)
+
+    list <- lapply(1:nrow(data_nested_parent), function(element_number) {
+
+      # Select relevant row
+      data_nested_child <- data_nested_parent[element_number, ]
+
+      # Build list
+      list <- list()
+      list[["language"]] <- data_nested_child$language
+      list[[data_nested_child$name_list_element]] <- data_nested_child$value
+
+      return(list)
+
+    })
+
+    # Rename list elements
+    names(list) <- rep(unique(data_nested_parent$name_list_element), length(list))
+
+    return(list)
+
+  })
+
+
+  # Split Up -------------------------------------------------------------------
+
+
+  # Define names
+  nested_list_names <- unique(data_nested$name_parent_element)
+
+  for (i in 1:length(nested_list_names)) {
+    assign(nested_list_names[i], list[[i]])
+  }
+
+
+  # PREPARE ADDRESS ============================================================
+
+
+  # Country List ---------------------------------------------------------------
+
+
+  country <- list(
+    "countryId" = list(data$country_countryId),
+    "countryIdISO2" = list(data$country_countryIdISO2),
+    "countryNameShort" = list(data$country_countryNameShort)
+  )
+
+
+
+
+  # STAND HIER
+
+
+
+
+  # PREPARE ORIGIN =============================================================
+
+
+  # PUT TOGETHER ===============================================================
+
+
+
+
+}
 
 
 
