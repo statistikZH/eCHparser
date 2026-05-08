77ac82b8cbf81a4484683e0c4822677713197fb1
Simon Graf <simon.graf@statistik.ji.zh.ch>
2026-05-08 14:18:21 +0200

    Update unit test for additional files

--- tests/testthat/test-build_eCH_0157.R
+++ tests/testthat/test-build_eCH_0157.R
@@ -1,7 +1,10 @@
 test_that("Built eCH-0157 file output is an XML and matches corresponding file", {
 
-  # Get the list of parsed test files
-  testfiles <- list.files(testthat::test_path("testdata/expected/eCH-0157"))
+  # Get the list of tabular .RDS files (that were parsed from original eCH-0157 files)
+  input_files <- list.files(testthat::test_path("testdata/expected/eCH-0157"))[grep(".RDS", list.files(testthat::test_path("testdata/expected/eCH-0157")))]
+
+  # Get the list of expected output files (from the same folder)
+  expected_files <- list.files(testthat::test_path("testdata/expected/eCH-0157"))[grep(".xml", list.files(testthat::test_path("testdata/expected/eCH-0157")))]
 
   # Define election types for test files
   election_types <- c("Majority", "Proportion")
@@ -12,20 +15,17 @@ test_that("Built eCH-0157 file output is an XML and matches corresponding file",
   # Initialize a list in a new environment to capture errors (new env is necessary to actually assign values inside of tryCatch)
   errorenv$errors <- list()
 
-  for (i in seq_along(testfiles)) {
+  for (i in seq_along(input_files)) {
 
     # Get the full file path
-    filepath <- testthat::test_path("testdata/expected/eCH-0157", paste0(testfiles[i]))
+    filepath <- testthat::test_path("testdata/expected/eCH-0157", paste0(input_files[i]))
 
     # Load and transform file
     file <- readRDS(filepath)
     file_out <- build_eCH_0157(file, election_types[i])
 
-    # # Ensure the output is a data frame
-    # testthat::expect_true(is.data.frame(file_out), info = paste("File:", testfiles[i]))
-
-    # Load the corresponding RDS file from the testdata folder (use testthat::test_path for the test to work also with devtools::test())
-    xml_filepath <- testthat::test_path("testdata/input/eCH-0157", paste0(sub("\\.[^.]*$", "", testfiles[i]), ".xml")) # the sub() removes the .xml extension
+    # Load the corresponding xml file from the same folder
+    xml_filepath <- testthat::test_path("testdata/expected/eCH-0157", paste0(expected_files[i]))
 
     if (file.exists(xml_filepath)) {
       expected_out <- xml2::read_xml(xml_filepath)
@@ -36,20 +36,20 @@ test_that("Built eCH-0157 file output is an XML and matches corresponding file",
         testthat::expect_equal(
           xml2::as_list(file_out)[[1]][[2]],
           xml2::as_list(expected_out)[[1]][[2]],
-          info = paste("File:", testfiles[i])
+          info = paste("File:", input_files[i])
         )
 
       }, error = function(e) {
 
         # Collect errors in the list for reporting later
-        errorenv$errors[[testfiles[i]]] <- e$message
+        errorenv$errors[[input_files[i]]] <- e$message
 
       })
 
     } else {
 
       # return a warning message if the corresponding RDS file was not found
-      warning(paste("No corresponding RDS file found for", testfiles[i]))
+      warning(paste("No corresponding RDS file found for", input_files[i]))
 
     }
 
